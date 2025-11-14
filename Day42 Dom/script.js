63520afa985d6f3d124072aec695ba118a13d866
shawkishan <161332646+shawkishan@users.noreply.github.com>
2025-11-14 12:06:12 +0530

    First Commit

---
+++ Day42 Dom/script.js
@@ -0,0 +1,9 @@
+let a=document.querySelector('h1')
+let ab=document.querySelector('h2')
+console.log(a)
+console.log(ab)
+
+var s=document.querySelector('h3')
+s.innerHTML='changed'
+
+h1.style.color='red'
