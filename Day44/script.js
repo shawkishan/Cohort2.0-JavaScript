bde7571ef190c3ff674cfd0bf8bc370e25fea501
shawkishan <161332646+shawkishan@users.noreply.github.com>
2025-11-18 11:53:40 +0530

    Second

---
+++ Day44/script.js
@@ -0,0 +1,15 @@
+var a=document.createElement('h1')
+console.log(a)
+
+var b=document.querySelector('button')
+
+b.addEventListener('click',function(){
+    var a=document.createElement('h1')
+     //console.log(a)
+     a.innerHTML="wef"
+     console.log(a)
+    //console.log("aef")
+    var boby=document.querySelector('boby')
+    boby.appendChild(h1)
+
+})
