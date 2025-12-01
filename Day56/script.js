0e94bf57c0f191e78ba0311f107d937ab6a657f6
shawkishan <161332646+shawkishan@users.noreply.github.com>
2025-12-01 21:46:33 +0530

    first

---
+++ Day56/script.js
@@ -0,0 +1,29 @@
+class Remot{
+    constructor(a,b,c){
+        this.a=a
+        this.b=b
+        this.c=c
+    }
+    poweron(){
+        console.log("asfas")
+    }
+    powerof(){
+        console.log("agsd")
+    }
+
+}
+let re= new Remot("asf",12,"aqef")
+
+class Kitab{
+    constructor(name,price,autor,color){
+        this.name=name
+        this.price=price
+        this.autor=autor
+        this.color=color
+    }
+     read(){
+        console.log("BOOK is read")
+     }
+}
+let firts=new Kitab("nna",23,"wegvvv","wrtwef")
+let seconds=new Kitab("nna",25,"wegvvv","wrtwef")
